a8868ed5d706e4a6bace09578889cd26e54bff26
Facundo Marin <facundo.marin@moxit.net>
2022-08-03 14:25:24 -0300

    26.66. Showing PopUp Windows

--- src/CustomMap..ts
+++ src/CustomMap..ts
@@ -24,22 +24,19 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
     });
-  }
 
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng,
-  //     },
-  //   });
-  // }
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'hi there!',
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
 }
