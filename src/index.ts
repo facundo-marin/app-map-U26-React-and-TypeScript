45c7d47b56b5687a27c4f90aa8f0c9f1b080bed2
Facundo Marin <facundo.marin@moxit.net>
2022-08-03 12:23:38 -0300

    26.62. Duplicate Code.

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,8 @@ import { Company } from './Company';
 import { CustomMap } from './CustomMap.';
 
 const user = new User();
-console.log(user);
-
 const company = new Company();
-console.log(company);
-
-// new CustomMap('map');
 const customMap = new CustomMap('map');
 
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
